324b4f43c44fc5e0816873196db8a76a3e0e7654
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-06-07 11:42:43 +0300

    fix: prevent truncation of short descriptions in truncateDescription helper

--- src/helpers/truncateDescription.ts
+++ src/helpers/truncateDescription.ts
@@ -1,4 +1,4 @@
 const truncateDescription = (description: string, maxChars: number = 210) =>
-  `${description.slice(0, maxChars)}...`;
+  description.length > maxChars ? `${description.slice(0, maxChars)}...` : description;
 
 export default truncateDescription;
