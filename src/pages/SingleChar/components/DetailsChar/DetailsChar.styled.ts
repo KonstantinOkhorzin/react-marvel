23e3fb62e70fcdb4b0a19ca42fabf4c6fafcd2a1
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-08-18 17:40:01 +0300

    style: add responsiveness for SingleChar and SingleComic pages

--- src/pages/SingleChar/components/DetailsChar/DetailsChar.styled.ts
+++ src/pages/SingleChar/components/DetailsChar/DetailsChar.styled.ts
@@ -1,16 +1,28 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.section`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  column-gap: 30px;
   justify-content: space-between;
   align-items: start;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Content = styled.div`
   display: grid;
-  grid-template-columns: 293px 550px;
+  grid-template-columns: 293px 1fr;
   align-items: start;
   gap: 50px;
+  padding-top: 11px;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+    gap: 25px;
+  }
 `;
 
 export const Description = styled.p`
