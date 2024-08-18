6483450957ed9426ec39394ac10f90791b37ad97
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-08-18 17:30:03 +0300

    style: add responsiveness for AppBanner component

--- src/components/SubLayout/components/AppBanner/AppBanner.styled.ts
+++ src/components/SubLayout/components/AppBanner/AppBanner.styled.ts
@@ -12,6 +12,23 @@ export const Wrapper = styled.div`
   background-size: 152px 100px, 133px 100px;
   background-position: 45px, calc(100% - 25px);
   padding: 18px 0 18px 280px;
+  margin: 0;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.laptop}) {
+    width: calc(100% + 2 * 15px);
+    margin: 0 -15px;
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 0 0 18px 170px;
+    background-position: 15px, calc(100% - 15px) 15px;
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 0 0 18px 15px;
+    background-image: url(${avengersLogo});
+    background-position: calc(100% - 15px) 15px;
+  }
 `;
 
 export const Text = styled.p`
