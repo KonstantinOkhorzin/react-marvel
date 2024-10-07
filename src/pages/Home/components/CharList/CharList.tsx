539e1a0cf3f38d3465679b81f13615bdc8887788
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-10-07 11:45:54 +0300

    refactor: migrate from Redux Toolkit to RTK Query in CharList component

--- src/pages/Home/components/CharList/CharList.tsx
+++ src/pages/Home/components/CharList/CharList.tsx
@@ -1,68 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Typography, CircularProgress } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { Status } from '../../../../types';
 import CharListView from './components/CharListView';
 import { useAppDispatch } from '../../../../hooks';
-import {
-  fetchCharacters,
-  fetchMoreCharacters,
-  setPage,
-  selectCharacters,
-} from '../../../../redux/characters';
+import { selectCharacters, setOffset, setData } from '../../../../redux/characters/slice';
+import { useGetCharactersQuery } from '../../../../redux/characters/api';
+import { handleError } from '../../../../helpers';
+
+const charactersLimit = Number(import.meta.env.VITE_CHARACTERS_LIMIT) || 9;
+const defaultCharactersOffset = Number(import.meta.env.VITE_DEFAULT_CHARACTERS_OFFSET) || 210;
 
 const CharList = () => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
   const dispatch = useAppDispatch();
-  const { charList, page, status, error, isLoadingMore, canLoadMore } =
-    useSelector(selectCharacters);
+  const { charList, offset, canLoadMore } = useSelector(selectCharacters);
+  const { data, error, isLoading, isFetching } = useGetCharactersQuery(offset, {
+    skip:
+      charList.length > 0 &&
+      (offset === charList.length + defaultCharactersOffset - charactersLimit || !canLoadMore),
+  });
 
   useEffect(() => {
-    if (charList.length > 0) return;
+    if (!data) return;
 
-    dispatch(fetchCharacters(page));
-  }, [charList.length, dispatch, page]);
-
-  useEffect(() => {
-    if (page === 1 || currentPage !== page) return;
-
-    dispatch(fetchMoreCharacters(page));
-  }, [currentPage, dispatch, page]);
+    dispatch(setData(data));
+  }, [data, dispatch]);
 
   const loadMore = () => {
-    setCurrentPage(page + 1);
-    dispatch(setPage(page + 1));
+    dispatch(setOffset(offset + charactersLimit));
   };
 
-  switch (status) {
-    case Status.PENDING:
-      return <CircularProgress sx={{ margin: '0 auto' }} />;
+  return (
+    <>
+      {isLoading && charList.length === 0 && <CircularProgress sx={{ margin: '0 auto' }} />}
 
-    case Status.RESOLVED:
-    case Status.IDLE:
-      if (charList.length > 0) {
-        return (
-          <CharListView
-            charList={charList}
-            onLoadMore={loadMore}
-            isLoadingMore={isLoadingMore}
-            canLoadMore={canLoadMore}
-          />
-        );
-      }
-      break;
+      {charList.length > 0 && (
+        <CharListView
+          charList={charList}
+          onLoadMore={loadMore}
+          isLoadingMore={isFetching}
+          canLoadMore={canLoadMore}
+        />
+      )}
 
-    case Status.REJECTED:
-      return (
+      {error && (
         <Typography variant='h3' component='p' color='error' align='center'>
-          {error}
+          {handleError(error)}
         </Typography>
-      );
-
-    default:
-      return null;
-  }
+      )}
+    </>
+  );
 };
 
 export default CharList;
