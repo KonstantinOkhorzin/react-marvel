8e783278fd1d4f1fb860de0f622101cec614159e
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-10-07 12:03:19 +0300

    refactor: migrate from Redux Toolkit to RTK Query in SideBar component

--- src/pages/Home/components/SideBar/SideBar.tsx
+++ src/pages/Home/components/SideBar/SideBar.tsx
@@ -7,31 +7,37 @@ import SearchForm from './components/SearchForm';
 import Skeleton from './components/Skeleton';
 import { Wrapper, BlockWrapper } from './SideBar.styled';
 import { useAppDispatch } from '../../../../hooks';
-import { Status } from '../../../../types';
-import { selectCharacterDetails, fetchCharacterById } from '../../../../redux/characterDetails';
+import { selectCharId, selectChar, setSelectedChar } from '../../../../redux/characters/slice';
+import { useGetCharacterByIdOrNameQuery } from '../../../../redux/characters/api';
+import { handleError } from '../../../../helpers';
 
 const SideBar = () => {
   const dispatch = useAppDispatch();
-  const { selectedChar, selectedCharId, status, error } = useSelector(selectCharacterDetails);
+  const selectedCharId = useSelector(selectCharId);
+  const selectedChar = useSelector(selectChar);
+  const { data, error, isFetching } = useGetCharacterByIdOrNameQuery(
+    { id: selectedCharId },
+    { skip: selectedCharId === undefined }
+  );
 
   useEffect(() => {
-    if (selectedCharId === null || (selectedChar && selectedChar.id === selectedCharId)) return;
+    if (!data) return;
 
-    dispatch(fetchCharacterById(String(selectedCharId)));
-  }, [dispatch, selectedChar, selectedCharId]);
+    dispatch(setSelectedChar(data));
+  }, [data, dispatch, selectedCharId]);
 
   return (
     <Wrapper>
       <BlockWrapper>
-        {status === Status.IDLE && selectedChar === null && <Skeleton />}
-        {status === Status.PENDING && (
-          <CircularProgress sx={{ margin: '0 auto', display: 'block' }} />
-        )}
-        {((status === Status.RESOLVED && selectedChar) ||
-          (status === Status.IDLE && selectedChar)) && <CharInfo char={selectedChar} />}
-        {status === Status.REJECTED && (
+        {!selectedChar && !isFetching && <Skeleton />}
+
+        {isFetching && <CircularProgress sx={{ margin: '0 auto', display: 'block' }} />}
+
+        {selectedChar && !isFetching && <CharInfo char={selectedChar} />}
+
+        {error && (
           <Typography variant='h3' component='p' color='error' align='center'>
-            {error}
+            {handleError(error)}
           </Typography>
         )}
       </BlockWrapper>
