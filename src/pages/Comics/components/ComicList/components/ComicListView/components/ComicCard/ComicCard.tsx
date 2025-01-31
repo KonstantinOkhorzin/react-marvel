8622d4ac7cafb0a8918b3cb39877dd9335148f58
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2025-01-31 12:01:15 +0200

    fix: add location state for navigation in ComicCard component

--- src/pages/Comics/components/ComicList/components/ComicListView/components/ComicCard/ComicCard.tsx
+++ src/pages/Comics/components/ComicList/components/ComicListView/components/ComicCard/ComicCard.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { IComic } from '../../../../../../../../types';
 import { Wrapper, Poster, Price, Title } from './ComicCard.styled';
@@ -9,8 +10,10 @@ interface Props {
 }
 
 const ComicCard: FC<Props> = ({ comic: { id, thumbnail, title, price } }) => {
+  const location = useLocation();
+  
   return (
-    <Wrapper to={`/${ROUTES.COMICS}/${id}`}>
+    <Wrapper to={`/${ROUTES.COMICS}/${id}`} state={{ from: location }}>
       <Poster src={thumbnail} alt='' />
       <Title>{title}</Title>
       <Price>{price}</Price>
